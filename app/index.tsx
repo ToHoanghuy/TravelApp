83612112f9cfb86bbbd82a78cea566b41117d048
1418299171 <22520573@gm.uit.edu.vn>
2025-05-24 00:44:31 +0700

    fix-route

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { RootStackParamList } from '@/types/navigation'
@@ -14,7 +14,7 @@ import VoucherScreen from '@/screens/VoucherScreen'
 import AddNewCollectionScreen from '@/screens/AddNewCollectionScreen'
 import DetailScreen from '@/screens/HomeScreen/DetailScreen'
 (Text as any).defaultProps = (Text as any).defaultProps || {};
-(Text as any).defaultProps.style = { fontFamily: 'UTMTimesBold'};
+(Text as any).defaultProps.style = { fontFamily: 'UTMTimesBold' };
 
 import React from 'react'
 import AvailableRoomScreen from '@/screens/AvailableRoomScreen'
@@ -28,87 +28,87 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
   return (
-    
-      <NavigationContainer >
-        <UserProvider>
-        <Stack.Navigator
-            initialRouteName='login'>
-                <Stack.Screen 
-                name = "welcome1" 
-                component={WelcomeScreen1}
-                options={{
-                headerShown: false,
-                headerTransparent:true,
-                }}/>
-                <Stack.Screen 
-                name = "login" 
-                component={LoginScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen 
-                name = "register" 
-                component={RegisterScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen 
-                name = "register2" 
-                component={RegisterScreen2}
-                options={{
-                headerShown: false
-                }}/>
 
-            <Stack.Screen 
-                name = "main-screen"
-                component={MainScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen 
-                name = "notifications-screen"
-                component={NotificationsScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen 
-                name = "personal-information-screen"
-                component={PersonalInformationScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen 
-                name = "voucher-screen"
-                component={VoucherScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen 
-                name = "add-new-collection-screen"
-                component={AddNewCollectionScreen}
-                options={{
-                headerShown: false
-                }}/>
-                <Stack.Screen
-                  name='detail-screen'
-                  component={DetailScreen}
-                  options={{
-                    headerShown: false
-                  }} />
-                  <Stack.Screen
+    <NavigationContainer >
+      <UserProvider>
+        <Stack.Navigator
+          initialRouteName='login'>
+          <Stack.Screen
+            name="welcome1"
+            component={WelcomeScreen1}
+            options={{
+              headerShown: false,
+              headerTransparent: true,
+            }} />
+          <Stack.Screen
+            name="login"
+            component={LoginScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name="register"
+            component={RegisterScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name="register2"
+            component={RegisterScreen2}
+            options={{
+              headerShown: false
+            }} />
+
+          <Stack.Screen
+            name="main-screen"
+            component={MainScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name="notifications-screen"
+            component={NotificationsScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name="personal-information-screen"
+            component={PersonalInformationScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name="voucher-screen"
+            component={VoucherScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name="add-new-collection-screen"
+            component={AddNewCollectionScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
+            name='detail-screen'
+            component={DetailScreen}
+            options={{
+              headerShown: false
+            }} />
+          <Stack.Screen
             name="available-room-screen"
             component={AvailableRoomScreen}
             options={{
               headerShown: false
             }} />
 
-            <Stack.Screen
+          <Stack.Screen
             name="chat-board-screen"
             component={ChatBoardScreen}
             options={{
               headerShown: false
             }} />
-            
+
           <Stack.Screen
             name="chat-screen"
             component={ChatScreen}
@@ -116,32 +116,32 @@ export default function App() {
               headerShown: false
             }} />
 
-                      <Stack.Screen
+          <Stack.Screen
             name='lucky-wheel-screen'
             component={LuckyWheelScreen}
             options={{
               headerShown: false
             }} />
 
-            <Stack.Screen
+          <Stack.Screen
             name='search-location-screen'
             component={SearchLocationScreen}
             options={{
               headerShown: false
             }} />
 
-            <Stack.Screen
+          <Stack.Screen
             name='view-map-screen'
             component={ViewMapScreen}
             options={{
               headerShown: false
             }} />
 
-            </Stack.Navigator>
-        </UserProvider>
-            
-      </NavigationContainer>
-    
-   
+        </Stack.Navigator>
+      </UserProvider>
+
+    </NavigationContainer>
+
+
   );
 }
